1a503a347db14e0eff2afb9fed9c393709a4be63
Bromand <romat3422@gamil.com>
2026-04-01 12:14:40 +0700

    исправления + музыка

--- engine/character.py
+++ engine/character.py
@@ -286,7 +286,7 @@ class ListCharacters:
             "shak" : _Character("Жаклин", "shak", "#f5a889", "#f0855b"),
             "masorubka" : _Character("Кассир", "masorubka", "#E0FFFF", "#7FFFD4"),
             "ed" : _Character("Сосед", "ed", "#FFDEAD", "#FFDEAD"),
-            "oz": _Character("ОЗИНАЖ", "oz", "#12d3da"),
+            "oz": _Character("Ф. ОЗИНАД", "oz", "#12d3da"),
         }
 
     def __getitem__(self, item) -> dict[str : Character]:
